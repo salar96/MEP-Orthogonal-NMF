2fe0b5b9c35710ecbb340062baa278e72f37f8f3
Salar Basiri <50495107+salar96@users.noreply.github.com>
2023-01-05 18:04:47 -0600

    Update onmf_DA.py

--- onmf_DA.py
+++ onmf_DA.py
@@ -1,21 +1,19 @@
 import numpy as np
-from utils import divide, normalize
 from Deterministic_Annealing import DA
 
 class ONMF_DA:
     name = "ONMF-DA"
-    def func (X, k,alpha=2,purturb=0.0001,verbos=0,normalize=True,auto_weighting=True,tol=1e-3):
+    def func (X, k,alpha=2,purturb=0.0001,verbos=0,normalize=True,auto_weighting=True,tol=1e-3,express=True):
         m, n = np.shape(X)
         model=DA(k,alpha=alpha,purturb=purturb,verbos=verbos,normalize=normalize,tol=tol)
         if auto_weighting:
             model.fit(X,Px='auto');
         else:
             model.fit(X);
-        Y,P=model.cluster()
+        Y,P=model.cluster(express=express)
         w=np.round(P.copy())
         for i in range(n):
             id=np.where(w[:,i]==1)[0]
-            norm1 = np.linalg.norm(Y[:,id])
-            w[id,i]=(X[:,i].T @ Y[:,id])/norm1/norm1
+            w[id,i]=np.divide(np.matmul(X[:,i].T , Y[:,id]),np.square(np.linalg.norm(Y[:,id])))
 
         return Y, w.T,model
