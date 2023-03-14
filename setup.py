b658662a1ec9d6d33ac47cfa13afa72f50694dd0
Salar Basiri <50495107+salar96@users.noreply.github.com>
2023-03-14 16:01:35 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mep-onmf',
+    version='1.0.1',
+    description='description here',
+    author='Salar Basiri',
+    author_email='salarbsr.1996@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
